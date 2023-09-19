5d264dffe8c7ce0a151c461bd448c43e2365e133
Evgeny Grakovich <grakovich-e@yandex.by>
2023-09-19 23:35:04 +0300

    custom photoes

--- handlers/buttons.py
+++ handlers/buttons.py
@@ -9,17 +9,17 @@ from states import State
 async def send_welcome(message: types.Message, state: FSMContext):
     input = message.text
     if input == texts.text_btn:
-        with open('images/text_ru.jpg', 'rb') as photo:
+        with open('images/текст.jpg', 'rb') as photo:
             await message.answer_photo(photo, reply_markup=kb.texts_kb)
     elif input == texts.support_btn:
-        with open('images/support_ru.jpg', 'rb') as photo:
+        with open('images/Сапорт.jpg', 'rb') as photo:
             await message.answer_photo(photo)
         await message.answer(texts.support_link)
     elif input == texts.video_btn:
-        with open('images/video_ru.jpg', 'rb') as photo:
+        with open('images/Видео.jpg', 'rb') as photo:
             await message.answer_photo(photo, reply_markup=kb.videos_kb)
     elif input == texts.media_btn:
-        with open('images/media_ru.jpg', 'rb') as photo:
+        with open('images/соцсети.jpg', 'rb') as photo:
             await message.answer_photo(photo, reply_markup=kb.media_kb)
     elif input == texts.change_lan_btn:
         await message.answer(texts.finger_down, reply_markup=kb.choose_lan_kb)
