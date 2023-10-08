e3ce6c5eac4d8d403649b39ead3b3928d954495d
Evgeniy Grakovich <grakovich-e@yandex.by>
2023-10-08 18:13:30 +0300

    changed to video intro

--- handlers/commands.py
+++ handlers/commands.py
@@ -7,8 +7,8 @@ from states import State
 
 @dp.message_handler(commands=['start'], state="*")
 async def send_welcome(message: types.Message, state: FSMContext):
-    with open('images/start.jpg', 'rb') as photo:
-        await message.answer_photo(photo)
+    with open('images/gif.mp4', 'rb') as video:
+        await message.answer_video(video)
     await message.answer(texts.welcome)
     await message.answer(texts.finger_down, reply_markup=kb.choose_lan_kb)
     await State.choosing_lan.set()
