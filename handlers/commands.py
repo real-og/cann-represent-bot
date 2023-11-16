b6db63fae84c0551a18c785c061c1b1062869ac2
Evgeniy Grakovich <grakovich-e@yandex.by>
2023-11-16 03:34:25 +0300

    table adding

--- handlers/commands.py
+++ handlers/commands.py
@@ -4,6 +4,8 @@ from aiogram.dispatcher import FSMContext
 import texts
 import keyboards as kb
 from states import State
+import aiotable
+
 
 @dp.message_handler(commands=['start'], state="*")
 async def send_welcome(message: types.Message, state: FSMContext):
@@ -12,3 +14,4 @@ async def send_welcome(message: types.Message, state: FSMContext):
     await message.answer(texts.welcome)
     await message.answer(texts.finger_down, reply_markup=kb.choose_lan_kb)
     await State.choosing_lan.set()
+    await aiotable.append_user(message.from_user.id, message.from_user.username)
